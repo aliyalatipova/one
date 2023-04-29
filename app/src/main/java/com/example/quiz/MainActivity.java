2645bfb6701e91fbc59ea9061ed9a0675b52e869
Hp ZBook <aliyalatipova060706@gmail.com>
2023-04-29 19:40:13 +0300

    короче пытаюсб понять то такое бд и заччем оно

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -45,6 +47,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //totalQuestionsTextView.setText("Total questions" + totalQuestion);
 
+        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS users (true_wr TEXT, age INTEGER, UNIQUE(name))");
+        db.execSQL("INSERT OR IGNORE INTO users VALUES ('Tom Smith', 23), ('John Dow', 31);");
+
+        Cursor query = db.rawQuery("SELECT * FROM users;", null);
+        //TextView textView = findViewById(R.id.textView);
+        questionTextView.setText("");
+        while(query.moveToNext()){
+            String name = query.getString(0);
+            int age = query.getInt(1);
+            questionTextView.append("Name: " + name + " Age: " + age + "\n");
+        }
+        query.close();
+        db.close();
         loadNewQuestion();
 
     }
