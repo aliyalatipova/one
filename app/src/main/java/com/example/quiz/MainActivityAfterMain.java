a91c2f105c8c70b4fac2a42ddb9a7c36b1124fb1
Hp ZBook <aliyalatipova060706@gmail.com>
2023-05-05 20:24:40 +0300

    какаято нредо ересь

--- app/src/main/java/com/example/quiz/MainActivityAfterMain.java
+++ app/src/main/java/com/example/quiz/MainActivityAfterMain.java
@@ -11,7 +11,8 @@ import android.widget.TextView;
 public class MainActivityAfterMain extends AppCompatActivity {
     public LinearLayout layout_for_txt;
     public TextView textView_with_res; // в xml это textView4
-    String text;
+    //String text;
+    public int totalQ=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +20,11 @@ public class MainActivityAfterMain extends AppCompatActivity {
         setContentView(R.layout.activity_main_after_main);
 
         findById();
-        getInfFromMainAct();
-        textView_with_res.setText(text);
-        createTextView();
-        Button[] btn = new Button[3];
+
+        Bundle arguments = getIntent().getExtras();
+        textView_with_res.setText(arguments.get("hello").toString());// ну как бы в текстик пишем что у нас получилосьр5оп7куво
+        String res = arguments.get("res").toString();
+        createTextView(res);
 
 
 
@@ -35,21 +37,20 @@ public class MainActivityAfterMain extends AppCompatActivity {
         textView_with_res = findViewById(R.id.textView4);
     }
 
-    public void getInfFromMainAct(){
-        Bundle arguments = getIntent().getExtras();
-        String text = arguments.get("hello").toString(); // text для textView
-        String res = arguments.get("res").toString();
-        int totalQuestion = Integer.parseInt(arguments.get("totalQuestion").toString());
-    }
 
-    public void createTextView(){
-        TextView[] txt = new TextView[3];
-        for (int i = 0; i < 3; ++i) {
+
+    public void createTextView(String res){
+        totalQ = res.length(); //длина стоски из плюсов и минусов
+
+        TextView[] txt = new TextView[totalQ];
+        for (int i = 0; i < totalQ; ++i) {
+            char c = res.charAt(i);
+            String sim_of_res = String.valueOf(c);
 
 
             txt[i] = new TextView(this);
             txt[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            txt[i].setText("кнопка " + i);
+            txt[i].setText(QuestionAnswer.choices[i][0]);
             int id= View.generateViewId();
             txt[i].setId(id);
 
@@ -57,4 +58,8 @@ public class MainActivityAfterMain extends AppCompatActivity {
             layout_for_txt.addView(txt[i]);
         }
     }
+
+    //public find_false_index(){
+
+   // }
 }
